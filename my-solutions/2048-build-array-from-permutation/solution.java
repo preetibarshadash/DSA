c3fa048c2accce3192569c10b7abc0ea91db4e2d
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-01-11 05:57:56 +0000

    Sync LeetCode submission Runtime - 1 ms (99.47%), Memory - 45.4 MB (99.74%)

--- my-solutions/2048-build-array-from-permutation/solution.java
+++ my-solutions/2048-build-array-from-permutation/solution.java
@@ -1,9 +1,8 @@
 class Solution {
     public int[] buildArray(int[] nums) {
-        int[] ans =new int[nums.length];
+        int[] ans = new int[nums.length];
         for(int i =0;i<nums.length;i++){
-            int ele = nums[i];
-            ans[i] = nums[ele];
+            ans[i]=nums[nums[i]];
         }
         return ans;
     }
