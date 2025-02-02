881e0ec89cab5b6d8c9208195f5f6a41c73f7cf9
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-02-02 09:12:17 +0000

    Sync LeetCode submission Runtime - 1 ms (29.85%), Memory - 45.3 MB (99.93%)

---
+++ my-solutions/0268-missing-number/solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i];
+            if (correct < nums.length && nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j) {
+                return j;
+            }
+        }
+
+        return nums.length;
+    }
+
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
