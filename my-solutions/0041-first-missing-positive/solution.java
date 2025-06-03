63b73da20a82144d0d130d8a538fe99630bc3e62
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-06-03 12:41:41 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 56.8 MB (99.95%)

--- my-solutions/0041-first-missing-positive/solution.java
+++ my-solutions/0041-first-missing-positive/solution.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0;
         while (i < nums.length) {
             int correct = nums[i] - 1;
-            if (nums[i] < nums.length && nums[i] > 0 && nums[i] != nums[correct]) {
+            if (nums[i] > 0 && nums[i] <= nums.length && nums[i] != nums[correct]) {
                 int temp = nums[i];
                 nums[i] = nums[correct];
                 nums[correct] = temp;
