d2a8553a475fff3c2b37c3aad8736768128456fa
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-06-03 10:54:04 +0000

    Sync LeetCode submission Runtime - 5 ms (95.79%), Memory - 57.3 MB (98.06%)

--- my-solutions/0442-find-all-duplicates-in-an-array/solution.java
+++ my-solutions/0442-find-all-duplicates-in-an-array/solution.java
@@ -1,17 +1,17 @@
 class Solution {
     public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
         int i = 0;
         while (i < nums.length) {
-            int correctIndex = nums[i] - 1;
-            if (nums[i] != nums[correctIndex]) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
                 int temp = nums[i];
-                nums[i] = nums[correctIndex];
-                nums[correctIndex] = temp;
+                nums[i] = nums[correct];
+                nums[correct] = temp;
             } else {
                 i++;
             }
         }
-        List<Integer> ans = new ArrayList<>();
         for (int j = 0; j < nums.length; j++) {
             if (nums[j] != j + 1) {
                 ans.add(nums[j]);
