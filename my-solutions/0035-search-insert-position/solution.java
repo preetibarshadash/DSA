a907a7deb4225fcddf9e18ad4ef128165ef94ed3
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-07-22 13:30:58 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.2 MB (99.97%)

--- my-solutions/0035-search-insert-position/solution.java
+++ my-solutions/0035-search-insert-position/solution.java
@@ -1,18 +1,16 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-
-            if (nums[mid] == target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end){
+            int mid = start + (end - start)/2;
+            if(nums[mid] == target)
                 return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+            if(nums[mid]>target)
+                end = mid - 1;
+            else
+                start = mid + 1;
         }
-
-        return left;
+        return start;
     }
 }
