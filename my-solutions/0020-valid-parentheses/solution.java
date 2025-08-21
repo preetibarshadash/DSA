22be4e3e855c4b241bb17c347a06ccef6ea04325
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-08-21 07:52:09 +0000

    Sync LeetCode submission Runtime - 2 ms (98.27%), Memory - 41.9 MB (99.97%)

--- my-solutions/0020-valid-parentheses/solution.java
+++ my-solutions/0020-valid-parentheses/solution.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-
         for (char c : s.toCharArray()) {
             if (c == '(' || c == '{' || c == '[') {
                 stack.push(c);
             } else {
-                if (stack.isEmpty()) {
-                    return false;
+                if (c == ')') {
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return false;
+                    }
                 }
-                char top = stack.pop();
-                if ((c == ')' && top != '(') ||
-                        (c == '}' && top != '{') ||
-                        (c == ']' && top != '[')) {
-                    return false;
+                if (c == '}') {
+                    if (stack.isEmpty() || stack.pop() != '{') {
+                        return false;
+                    }
+                }
+                if (c == ']') {
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return false;
+                    }
                 }
             }
         }
-
         return stack.isEmpty();
     }
 }
