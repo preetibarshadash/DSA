06113de813ea400a29a1568397419bdba7bc8877
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-08-24 07:42:18 +0000

    Sync LeetCode submission Runtime - 1 ms (9.53%), Memory - 41.9 MB (99.98%)

---
+++ my-solutions/0101-symmetric-tree/solution.java
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
+        while (!q.isEmpty()) {
+            TreeNode a = q.poll();
+            TreeNode b = q.poll();
+            if (a == null && b == null)
+                continue;
+            if (a == null || b == null)
+                return false;
+            if (a.val != b.val)
+                return false;
+            q.add(a.left);
+            q.add(b.right);
+            q.add(a.right);
+            q.add(b.left);
+
+        }
+        return true;
+    }
+}
