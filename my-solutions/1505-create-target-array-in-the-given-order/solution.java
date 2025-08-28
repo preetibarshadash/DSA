206d75d879582f849dd6f3eb5117c9f7cd18bd5c
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-08-28 14:42:41 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.2 MB (99.96%)

--- my-solutions/1505-create-target-array-in-the-given-order/solution.java
+++ my-solutions/1505-create-target-array-in-the-given-order/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] createTargetArray(int[] nums, int[] index) {
-        int[] ans = new int[nums.length];
-        for(int n=0;n<nums.length;n++){
-            
-                int in = index[n];
-                if(ans[in]!=-1){
-                    for(int j = nums.length-1;j>in;j--){
-                        ans[j]=ans[j-1];
-                    }
-                }
-                ans[in]=nums[n];
-            
+        List<Integer> list = new ArrayList<>();
+        if (nums.length == 0 && index.length == 0)
+            return new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            list.add(index[i], nums[i]);
+        }
+        int[] ans = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            ans[i] = list.get(i);
         }
         return ans;
     }
