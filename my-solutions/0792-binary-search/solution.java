ce8109fa2180c46eb0a8d75cd83740374e749dc0
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-09-24 11:48:18 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.2 MB (99.86%)

--- my-solutions/0792-binary-search/solution.java
+++ my-solutions/0792-binary-search/solution.java
@@ -1,16 +1,19 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0;
-        int end = nums.length - 1;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] == target)
-                return mid;
-            if (nums[mid] > target)
-                end = mid - 1;
-            else
-                start = mid + 1;
+        return helper(nums, target, 0, nums.length - 1);
+    }
+
+    int helper(int[] arr, int target, int start, int end) {
+        if (start > end)
+            return -1;
+
+        int mid = (end - start) / 2 + start;
+        if (arr[mid] == target)
+            return mid;
+        if (arr[mid] > target) {
+            return helper(arr, target, start, mid - 1);
+        } else {
+            return helper(arr, target, mid + 1, end);
         }
-        return -1;
     }
 }
