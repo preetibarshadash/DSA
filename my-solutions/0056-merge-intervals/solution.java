ffa2433f9bcd12c0c9bd9f2a379a2f6fd7fd643a
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-10-11 11:28:43 +0000

    Sync LeetCode submission Runtime - 8 ms (91.97%), Memory - 46.6 MB (99.99%)

--- my-solutions/0056-merge-intervals/solution.java
+++ my-solutions/0056-merge-intervals/solution.java
@@ -1,18 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        ArrayList<int[]> ans = new ArrayList<>();
-        int[] currInterval = intervals[0];
+        int[] curr = intervals[0];
+        List<int[]> ans = new ArrayList<>();
         for (int i = 1; i < intervals.length; i++) {
-            int[] nextInterval = intervals[i];
-            if (nextInterval[0] <= currInterval[1]) {
-                currInterval[1] = Math.max(nextInterval[1], currInterval[1]);
+            int[] next = intervals[i];
+            if (next[0] <= curr[1]) {
+                curr[1] = Math.max(next[1], curr[1]);
             } else {
-                ans.add(currInterval);
-                currInterval = nextInterval;
+                ans.add(curr);
+                curr = next;
             }
         }
-        ans.add(currInterval);
+        ans.add(curr);
         return ans.toArray(new int[ans.size()][]);
     }
 }
