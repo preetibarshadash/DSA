332a1971aca900bd759aa803b31e587f215d8a51
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-10-14 05:07:56 +0000

    Sync LeetCode submission Runtime - 13 ms (20.55%), Memory - 45.1 MB (99.96%)

---
+++ my-solutions/0136-single-number/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int n : nums) {
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
