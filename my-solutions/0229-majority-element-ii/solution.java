3a88ef2c6cfa9d52092ab6e60953ef7d6fd5641d
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2025-11-05 13:35:33 +0000

    Sync LeetCode submission Runtime - 14 ms (41.11%), Memory - 51.2 MB (95.09%)

--- my-solutions/0229-majority-element-ii/solution.java
+++ my-solutions/0229-majority-element-ii/solution.java
@@ -1,14 +1,15 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+        List<Integer> ans = new ArrayList<>();
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > (n / 3))
+            if (entry.getValue() > (n / 3)) {
                 ans.add(entry.getKey());
+            }
         }
         return ans;
     }
