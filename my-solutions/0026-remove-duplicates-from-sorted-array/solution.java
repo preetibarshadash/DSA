090302475a764937a28df3a6e35680691dac079e
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-04-11 14:31:58 +0000

    Sync LeetCode submission Runtime - 1 ms (79.02%), Memory - 46.7 MB (43.46%)

--- my-solutions/0026-remove-duplicates-from-sorted-array/solution.java
+++ my-solutions/0026-remove-duplicates-from-sorted-array/solution.java
@@ -1,7 +1,6 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int i = 0;
-        int j = i + 1;
+        int i = 0, j = i + 1;
         while (j < nums.length) {
             if (nums[i] == nums[j]) {
                 j++;
@@ -11,6 +10,7 @@ class Solution {
                 j++;
             }
         }
+
         return i + 1;
     }
 }
