fbdf50b9c18e6c0fbaaf6f9dc515b997dac2fd0b
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-01 13:44:43 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 47.1 MB (87.39%)

--- my-solutions/1019-squares-of-a-sorted-array/solution.java
+++ my-solutions/1019-squares-of-a-sorted-array/solution.java
@@ -1,63 +1,19 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-
-        int n = nums.length;
-        int[] positives = new int[n];
-        int[] negatives = new int[n];
-        int p = 0;
-        int neg = 0;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] >= 0) {
-                positives[p] = nums[i];
-                p++;
+        int i = 0, n = nums.length, j = n - 1;
+        int[] ans = new int[n];
+        for (int k = n - 1; k >= 0; k--) {
+            int lowSqrd = nums[i] * nums[i];
+            int highSqrd = nums[j] * nums[j];
+            if (lowSqrd > highSqrd) {
+                ans[k] = lowSqrd;
+                i++;
             } else {
-                negatives[neg] = nums[i];
-                neg++;
+                ans[k] = highSqrd;
+                j--;
             }
         }
+        return ans;
 
-        for (int j = 0; j < p; j++) {
-            positives[j] = positives[j] * positives[j];
-        }
-
-        for (int k = 0; k < neg; k++) {
-            negatives[k] = negatives[k] * negatives[k];
-        }
-
-        reverse(negatives, 0, neg - 1);
-       
-
-        int[] result = new int[n];
-        int idx = 0;
-        int a = 0, b = 0;
-
-        while (a < p && b < neg) {
-            if (positives[a] < negatives[b]) {
-                result[idx] = positives[a];
-                idx++;
-                a++;
-            } else {
-                result[idx] = negatives[b];
-                idx++;
-                b++;
-            }
-        }
-
-        while (a < p) {
-            result[idx++] = positives[a++];
-        }
-        while (b < neg) {
-            result[idx++] = negatives[b++];
-        }
-        return result;
-    }
-
-    public void reverse(int[] arr, int start, int end) {
-        if (start > end)
-            return;
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        reverse(arr, start + 1, end - 1);
     }
 }
