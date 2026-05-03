66a988cdae14d59e08beb4a09a8287d100dbbdd8
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-03 10:16:02 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.6 MB (24.93%)

--- my-solutions/0075-sort-colors/solution.java
+++ my-solutions/0075-sort-colors/solution.java
@@ -1,21 +1,22 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n = nums.length;
-        int low = 0, mid = 0, high = n - 1;
+        int low = 0, mid = 0, n = nums.length, high = n - 1;
         while (mid <= high) {
             if (nums[mid] == 0) {
-                int temp = nums[low];
-                nums[low] = nums[mid];
-                nums[mid] = temp;
+                // swap(nums[mid], nums[low]);
+                int temp = nums[mid];
+                nums[mid] = nums[low];
+                nums[low] = temp;
                 low++;
                 mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
             } else if (nums[mid] == 2) {
+                // swap(nums[mid], nums[high]);
                 int temp = nums[mid];
                 nums[mid] = nums[high];
                 nums[high] = temp;
                 high--;
-            }else{
-                mid++;
             }
         }
     }
