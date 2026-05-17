38a094c43d5b7c0ef8ecfebd7ea253b60f0c96af
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-17 12:18:49 +0000

    Sync LeetCode submission Runtime - 33 ms (73.93%), Memory - 59.3 MB (45.71%)

--- my-solutions/0015-3sum/solution.java
+++ my-solutions/0015-3sum/solution.java
@@ -1,12 +1,10 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n = nums.length;
         List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
         Arrays.sort(nums);
-
         for (int i = 0; i < n - 2; i++) {
             int low = i + 1, high = n - 1;
-
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
@@ -18,15 +16,16 @@ class Solution {
                     high--;
                     while (low < high && nums[low] == nums[low - 1])
                         low++;
-                    while (low < high && nums[high] == nums[high + 1]) {
+                    while (low < high && nums[high] == nums[high + 1])
                         high--;
-                    }
+
                     continue;
                 }
                 if (sum > 0)
                     high--;
                 else
                     low++;
+
             }
         }
         return ans;
