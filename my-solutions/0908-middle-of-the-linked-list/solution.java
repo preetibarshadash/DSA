e2bdbce846b1cf9fefc240249ca382c1cd157fd1
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-21 13:37:45 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (54.51%)

--- my-solutions/0908-middle-of-the-linked-list/solution.java
+++ my-solutions/0908-middle-of-the-linked-list/solution.java
@@ -10,12 +10,12 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+        ListNode slow = head, fast = head;
         while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
         }
+
         return slow;
     }
 }
