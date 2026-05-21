f90147147e19e0b21e0ca9eb063ae82e0b8ae3fe
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-21 17:13:17 +0000

    Sync LeetCode submission Runtime - 1 ms (78.24%), Memory - 42.3 MB (79.70%)

--- my-solutions/0202-happy-number/solution.java
+++ my-solutions/0202-happy-number/solution.java
@@ -1,24 +1,23 @@
 class Solution {
     public boolean isHappy(int n) {
-        int fast = n;
-        int slow = n;
-        do {
-            slow = findSquare(slow);
-            fast = findSquare(findSquare(fast));
-        } while (fast != slow);
-        if (slow == 1) {
-            return true;
+        int slow = n, fast = n;
+        while (fast != 1) {
+            slow = getSum(slow);
+            fast = getSum(fast);
+            fast = getSum(fast);
+            if (slow == fast && fast != 1)
+                return false;
         }
-        return false;
+        return true;
     }
 
-    public int findSquare(int n) {
-        int ans = 0;
+    public int getSum(int n) {
+        int sum = 0;
         while (n > 0) {
             int dig = n % 10;
-            ans += dig * dig;
+            sum += dig * dig;
             n /= 10;
         }
-        return ans;
+        return sum;
     }
 }
