6d06a83d77788a2eb3e6d1e3eb87bce1f367d0e9
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-21 11:48:14 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 46.7 MB (52.31%)

--- my-solutions/0141-linked-list-cycle/solution.java
+++ my-solutions/0141-linked-list-cycle/solution.java
@@ -11,15 +11,14 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
+        ListNode fast = head, slow = head;
         while (fast != null && fast.next != null) {
-            fast = fast.next.next;
             slow = slow.next;
-            if (fast == slow) {
+            fast = fast.next.next;
+            if (fast == slow)
                 return true;
-            }
         }
+
         return false;
     }
 }
