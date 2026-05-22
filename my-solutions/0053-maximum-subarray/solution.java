414fccb1faefc32c3d731f63fc41616c048513c6
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-22 11:22:11 +0000

    Sync LeetCode submission Runtime - 1 ms (99.97%), Memory - 77.5 MB (19.32%)

--- my-solutions/0053-maximum-subarray/solution.java
+++ my-solutions/0053-maximum-subarray/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
+        int localMax = nums[0];
+        int max = nums[0];
         for (int i = 1; i < nums.length; i++) {
-            currentSum = Math.max(nums[i], currentSum + nums[i]);
-            maxSum = Math.max(currentSum, maxSum);
+            localMax = Math.max(nums[i], nums[i] + localMax);
+            max = Math.max(max, localMax);
         }
-        return maxSum;
+
+        return max;
     }
 }
