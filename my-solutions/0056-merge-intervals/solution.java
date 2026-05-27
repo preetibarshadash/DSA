5ac018332461e859af5cdef7c3f0557688938124
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-05-27 15:20:23 +0000

    Sync LeetCode submission Runtime - 8 ms (91.97%), Memory - 49.5 MB (6.20%)

--- my-solutions/0056-merge-intervals/solution.java
+++ my-solutions/0056-merge-intervals/solution.java
@@ -1,18 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-        int[] curr = intervals[0];
-        List<int[]> ans = new ArrayList<>();
+
+        List<int[]> mergedList = new ArrayList<>();
+
+        int start1 = intervals[0][0];
+        int end1 = intervals[0][1];
+
         for (int i = 1; i < intervals.length; i++) {
-            int[] next = intervals[i];
-            if (next[0] <= curr[1]) {
-                curr[1] = Math.max(next[1], curr[1]);
+            int start2 = intervals[i][0];
+            int end2 = intervals[i][1];
+
+            if (end1 >= start2) {
+                end1 = Math.max(end1, end2);
             } else {
-                ans.add(curr);
-                curr = next;
+
+                mergedList.add(new int[] { start1, end1 });
+                start1 = start2;
+                end1 = end2;
             }
         }
-        ans.add(curr);
-        return ans.toArray(new int[ans.size()][]);
+
+        mergedList.add(new int[] { start1, end1 });
+        return mergedList.toArray(new int[mergedList.size()][]);
+
     }
 }
