cb13b48000b60401dddf68e3a1be8e94dd3c1611
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-07-09 12:45:19 +0000

    Sync LeetCode submission Runtime - 2 ms (95.67%), Memory - 48.4 MB (42.25%)

--- my-solutions/0167-two-sum-ii---input-array-is-sorted/solution.java
+++ my-solutions/0167-two-sum-ii---input-array-is-sorted/solution.java
@@ -3,13 +3,14 @@ class Solution {
         int n = numbers.length, low = 0, high = n - 1;
         while (low < high) {
             int sum = numbers[low] + numbers[high];
-            if (sum == target)
+            if (sum == target) {
                 return new int[] { low + 1, high + 1 };
-            if (sum > target)
+            } else if (sum > target) {
                 high--;
-            else
+            } else
                 low++;
         }
+
         return new int[] { -1, -1 };
     }
 }
