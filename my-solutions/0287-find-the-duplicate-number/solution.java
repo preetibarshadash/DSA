1b057d45e844b34c9cb6313831b69f4fa44c474b
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-07-10 07:51:10 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 85.9 MB (23.38%)

--- my-solutions/0287-find-the-duplicate-number/solution.java
+++ my-solutions/0287-find-the-duplicate-number/solution.java
@@ -1,18 +1,13 @@
 class Solution {
     public int findDuplicate(int[] nums) {
-        int fast = 0, slow = 0, n = nums.length;
-        while (true) {
-            fast = nums[fast];
-            fast = nums[fast];
-            slow = nums[slow];
-            if (fast == slow) {
-                slow = 0;
-                while (slow != fast) {
-                    slow = nums[slow];
-                    fast = nums[fast];
-                }
-                return slow;
+        int n = nums.length;
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            if (visited[nums[i]]) {
+                return nums[i];
             }
+            visited[nums[i]] = true;
         }
+        return -1;
     }
 }
