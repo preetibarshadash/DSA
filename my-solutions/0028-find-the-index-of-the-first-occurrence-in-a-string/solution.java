6f754428f5bbd9279cacb43a72ffbaba89001449
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-07-26 13:57:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (9.12%)

--- my-solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
+++ my-solutions/0028-find-the-index-of-the-first-occurrence-in-a-string/solution.java
@@ -2,12 +2,9 @@ class Solution {
     public int strStr(String haystack, String needle) {
         int n = haystack.length();
         int l = needle.length();
-
-        if (needle.isEmpty())
-            return -1;
-
         for (int i = 0; i <= n - l; i++) {
-            if (haystack.substring(i, i + l).equals(needle))
+            if (needle.equals(haystack.substring(i, i
+                    + l)))
                 return i;
         }
         return -1;
