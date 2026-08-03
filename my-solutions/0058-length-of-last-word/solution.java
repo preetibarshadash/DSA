cb060f3766ffa1c96760360e35558915dbf80400
Preetibarsha Dash <103323658+preetibarshadash@users.noreply.github.com>
2026-08-03 12:55:23 +0000

    Sync LeetCode submission Runtime - 1 ms (30.46%), Memory - 42.8 MB (84.79%)

--- my-solutions/0058-length-of-last-word/solution.java
+++ my-solutions/0058-length-of-last-word/solution.java
@@ -1,17 +1,6 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int length = 0;
-        int i = s.length() - 1;
-
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
-        }
-
-        while (i >= 0 && s.charAt(i) != ' ') {
-            length++;
-            i--;
-        }
-
-        return length;
+        String[] word = s.split(" ");
+        return word[word.length - 1].length();
     }
 }
